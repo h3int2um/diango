18773a214aa02dd1bed8c240b499cc6447ac9b8a
h3int2um <h3int2um@gmail.com>
2016-09-27 23:37:18 +0700

    Script kiem tra version Django

---
+++ django-tutorials/code-django-tutorials/django-version.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+#
+# Thuc hien: Thi Minh Nhut -- Email: thiminhnhut@gmail.com
+# Thoi gian: Ngay 27 thang 09 nam 2016
+# Kiem tra phien ban cua Django
+#
+
+import django
+
+print "Django version: " + django.__version__
